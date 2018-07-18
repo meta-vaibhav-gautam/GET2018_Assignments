1dfe10832c2851e7c8f76a6b4ab042a3117613db
meta-vaibhav-gautam <vaibhav.gautam@metacube.com>
2018-07-18 17:19:19 +0530

    refined file structure with assignment

---
+++ Assignments/SCF_Session-2/SCF_Assignment-1/src/HexCalcMethods.java
@@ -0,0 +1,13 @@
+
+public interface HexCalcMethods {
+
+	public String add(String firstNumber, String secondNumber);
+	public String subtract(String firstNumber, String secondNumber);
+	public String multiply(String firstNumber, String secondNumber);
+	public String divide(String firstNumber, String secondNumber);
+	public boolean equal(String firstNumber, String secondNumber);
+	public boolean greaterThan(String firstNumber, String secondNumber);
+	public boolean lessThan(String firstNumber, String secondNumber);
+	public String hexToDecimal(String number);
+	public String decimalToHex(String number);
+}

b07c0e3000c9a7c789ba49dc65210d87e6111585
meta-vaibhav-gautam <vaibhav.gautam@metacube.com>
2018-07-23 16:48:54 +0530

    Searching: different test cases added for linear and binary search

--- Assignments/SCF_Session-5/SCF_Session-5_Q2/searchingInArrayTest.java
+++ Assignments/SCF_Session-5/SCF_Session-5_Q2/searchingInArrayTest.java
@@ -6,23 +6,39 @@ import org.junit.Test;
 public class searchingInArrayTest {
 
 	@Test
-	public void testPass() {
+	public void testPassLinearSearch() {
 		int arr[]={2,5,7,8,10,12};
 		SearchingInArray obj=new SearchingInArray();
 		int resultLinearSearch=obj.linearSearch(arr,8);
-		int resultBinarySearch=obj.binarySearch(arr, 0, arr.length, 8);
 		
 		assertEquals(3, resultLinearSearch);
 	}
 
 	@Test
-	public void testFail() {
+	public void testFailLinearSearch() {
 		int arr[]={2,5,7,8,10,12};
 		SearchingInArray obj=new SearchingInArray();
 		int resultLinearSearch=obj.linearSearch(arr,8);
+		
+		assertNotEquals(4, resultLinearSearch);
+	}
+	
+	@Test
+	public void testPassBinarySearch() {
+		int arr[]={2,5,7,8,10,12};
+		SearchingInArray obj=new SearchingInArray();
 		int resultBinarySearch=obj.binarySearch(arr, 0, arr.length, 8);
 		
-		assertEquals(4, resultLinearSearch);
+		assertEquals(3, resultBinarySearch);
+	}
+
+	@Test
+	public void testFailBinarySearch() {
+		int arr[]={2,5,7,8,10,12};
+		SearchingInArray obj=new SearchingInArray();
+		int resultBinarySearch=obj.binarySearch(arr, 0, arr.length, 8);
+		
+		assertNotEquals(4, resultBinarySearch);
 	}
 
 }

45a7121184b783a7656fb32b9df83c1ddde59992
meta-vaibhav-gautam <vaibhav.gautam@metacube.com>
2018-07-25 11:31:57 +0530

    minor changes with length variable

--- Assignments/SCF_Session-4/SCF_Session-4_Q1/LargestMirrorSection.java
+++ Assignments/SCF_Session-4/SCF_Session-4_Q1/LargestMirrorSection.java
@@ -18,15 +18,15 @@ public class LargestMirrorSection {
         }
         else
         {
-	        int reverseArray[] = new int[arrSequence.length]; //reverse array of the input array
-	        int length = arrSequence.length;
+        	int length = arrSequence.length;
+        	int reverseArray[] = new int[length]; //reverse array of the input array
 	        int arr[][] = new int[length + 1][length + 1]; //2-D array used for comparing arrays
 	        int index = 0;
-	        for (int i = arrSequence.length - 1; i >= 0; i--) {
+	        for (int i = length - 1; i >= 0; i--) {
 	            reverseArray[index] = arrSequence[i];
 	            index++;
 	        }
-	        for (int i = 0; i < arrSequence.length; i++) {
+	        for (int i = 0; i <length; i++) {
 	            for (int j = 0; j < reverseArray.length; j++) {
 	                if (arrSequence[i] == reverseArray[j]) {
 	                    arr[i + 1][j + 1] = arr[i][j] + 1;
